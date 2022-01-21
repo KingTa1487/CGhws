160eb41b50f7c0cba3fec4888b2be562cfc49364
KingTa <096084@jyes.ntpc.edu.tw>
2022-01-22 03:23:41 +0800

    vvv

--- HW5/teapot1.js
+++ HW5/teapot1.js
@@ -3,12 +3,11 @@ import { OrbitControls } from "https://threejs.org/examples/jsm/controls/OrbitCo
 import {TeapotGeometry} from "https://threejs.org/examples/jsm/geometries/TeapotGeometry.js";
 
 var scene, renderer, camera;
-var teapots=[], pointLight;
+
+var teapots=[], pointLight, cone = new THREE.Group(), light;
 var turn = true;
 var angle = 0;
 
-init();
-animate();
 
 function init() {
     var width = window.innerWidth;
@@ -28,14 +27,20 @@ function init() {
     camera.position.z = 400;
 
     let controls = new OrbitControls(camera, renderer.domElement);
-    window.addEventListener('resize', onWindowResize, false);
+
 	
     var gridXZ = new THREE.GridHelper(200, 20, 'red', 'white');
     scene.add(gridXZ);
 
     pointLight = new THREE.PointLight(0xffffff);
-    scene.add (pointLight);
-    scene.add (new THREE.PointLightHelper(pointLight, 10));
+    //scene.add (pointLight);
+    //scene.add (new THREE.PointLightHelper(pointLight, 10));
+	light = new THREE.Mesh(new THREE.SphereGeometry( 10, 32, 16 ),new THREE.MeshBasicMaterial( { color: 0xffff00 } ));
+	light.position.y = -13;
+	cone = new THREE.Mesh(new THREE.CylinderGeometry( 5, 5, 10, 32 ), new THREE.MeshBasicMaterial( {color: 0x000000} ) );
+	cone.add(light);
+	scene.add(cone);
+	//pointLight.add(cone);
 
 	var ambientLight = new THREE.AmbientLight(0x111111);
     scene.add(ambientLight);
@@ -62,9 +67,11 @@ function init() {
 		}
 	}
     
+	
+	window.addEventListener('resize', onWindowResize, false);
     
     //mesh.position.set (70, 0, 50);
-    //mesh.rotation.y = Math.PI/7;
+    
 
 }
 
@@ -76,12 +83,13 @@ function onWindowResize() {
 
 function animate() {
  
-    angle += 0.01;
+    if (turn) angle += 0.01;
     
-    pointLight.position.set(50 * Math.cos(angle), 80, 50 * Math.sin(angle));    
-    teapots[0].material.uniforms.lightpos.value.copy (pointLight.position);
+    cone.position.set(80 * Math.cos(angle), 50,  80* Math.sin(angle));    
+    
 
     teapots.forEach(function(run){
+		run.material.uniforms.lightpos.value.copy (cone.position);
 		run.rotation.y = 1.3 * angle;
 	})
 	
